efeea573ea1e612c4d2ffe05d09909ea6d8c2504
Dhanush0605 <dhanushd.bsc23@rvu.edu.in>
2024-02-23 13:52:40 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -3,8 +3,8 @@
 int main()
 {
     int a , b , sum;
-    printf("enter two numbers");
+    printf("enter two numbers:\n");
     scanf("%d %d",&a,&b);
-    printf("the sum of %dd + %d is %d" , a, b, a+b);
+    printf("the sum of %d + %d is %d" , a, b, a+b);
     return 0;
 }
