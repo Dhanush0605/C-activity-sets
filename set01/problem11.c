8a9ef716b2db539629dcce322b391b565e1e20dd
Dhanush0605 <dhanushd.bsc23@rvu.edu.in>
2024-02-26 14:22:02 +0530

    test

--- set01/problem11.c
+++ set01/problem11.c
@@ -1,92 +1,37 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
-=======
-#include<stdio.h>
->>>>>>> c1ffc74eeaa9059431174949411a077e37e8890d
-struct _complex {
-    float real;
-    float imaginary;
-};
+int main(void) {
 
-typedef struct _complex Complex;
+int i;
+char ch1[100],ch2[100];
 
-<<<<<<< HEAD
-Complex input_complex() {
-    Complex c;
-    printf("Enter real part: ");
-    scanf("%f", &c.real);
-    printf("Enter imaginary part: ");
-    scanf("%f", &c.imaginary);
-    return c;
+  printf("enter the first string:\n");
+scanf("%s",&ch1);
+
+  printf("enter the second string:\n");
+  scanf("%s",&ch2);
+
+
+for(int i=0 ;ch1[i] == ch2[i]&&ch1[i]!='\0'&&ch2[i]!='\0';i++);
+
+if(ch1[i]>ch2[i])
+{
+printf("%s is greater\n",ch1);
+  return 0;
+  
 }
 
-Complex add_complex(Complex a, Complex b) {
-    Complex sum;
-    sum.real = a.real + b.real;
-    sum.imaginary = a.imaginary + b.imaginary;
-    return sum;
-}
+  if(ch2[i]>ch1[i])
+  {
+  printf("%s is greater\n",ch1);
+    return 0;
 
-void output(Complex a, Complex b, Complex sum) {
-    printf("Complex A: %.2f + %.2fi\n", a.real, a.imaginary);
-    printf("Complex B: %.2f + %.2fi\n", b.real, b.imaginary);
-    printf("Sum: %.2f + %.2fi\n", sum.real, sum.imaginary);
-}
+  }
 
-int main() {
-    Complex complexA, complexB, result;
-
-    printf("Enter values for Complex A:\n");
-    complexA = input_complex();
-
-    printf("Enter values for Complex B:\n");
-    complexB = input_complex();
-
-    result = add_complex(complexA, complexB);
-
-    printf("Result:\n");
-    output(complexA, complexB, result);
-
+  else
+  {
+    printf("strings are equal\n");
+  }
 return 0;
 }
-=======
-Complex input_complex();
-Complex add_complex(Complex a, Complex b);
-void output(Complex a, Complex b, Complex sum);
 
-    int main() {
-    Complex num1, num2, sum;
-
-    printf("Enter the first complex number:\n");
-    num1 = input_complex(); 
-    printf("\nEnter the second complex number:\n");
-    num2 = input_complex(); 
-
-    sum = add_complex(num1, num2); 
-
-    output(num1, num2, sum); 
-
-    return 0;
-}
-    Complex input_complex() {
-    Complex num;
-    printf("Enter real part: ");
-    scanf("%f", &num.real);
-    printf("Enter imaginary part: ");
-    scanf("%f", &num.imaginary);
-    return num;
-}
-
-    Complex add_complex(Complex a, Complex b) {
-    Complex result;
-    result.real = a.real + b.real;
-    result.imaginary = a.imaginary + b.imaginary;
-    return result;
-}
-
-    void output(Complex a, Complex b, Complex sum) {
-    printf("\n Sum of ");
-    printf("(%.2f + %.2fi) and ", a.real, a.imaginary);
-    }
->>>>>>> c1ffc74eeaa9059431174949411a077e37e8890d
