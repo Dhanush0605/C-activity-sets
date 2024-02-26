4e3a62d22e738b6a88ce031f0c998e23ba935023
Dhanush0605 <dhanushd.bsc23@rvu.edu.in>
2024-02-26 14:11:39 +0530

    test

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-<<<<<<< HEAD
 int input_n();
 int sum_n_nos(int n,int sum);
 void output(int n, int sum);
@@ -29,31 +28,5 @@ int sum_n_nos(int n,int sum)
 }
 void output(int n, int sum)
 {
-    printf("The sum of %d is %d\n",n,sum);
-=======
-int input_n()
-{
-    int n;
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
-    return n;
-}
-   int sum_n_nos(int n){
-   int sum=0;
-   for (int i = 1; i <= n; i++) {
-   sum += i;
-    }
-   return sum;
-}
-
-    void output(int n, int sum) {
-    printf("The sum of natural numbers up to %d is: %d\n", n, sum);
-}
-
-    int main() {
-    int num = input_n();
-    int sum = sum_n_nos(num);
-    output(num, sum);
-    return 0;
->>>>>>> c1ffc74eeaa9059431174949411a077e37e8890d
+    printf("The sum of %d is %d\n",n,sum);
 }
